154d9bf8ab55af5dd90e26755a4db254bc4fc047
Michael.Kryvoruchko <Michael.Kryvoruchko@mdm.de>
2021-12-05 23:55:30 +0200

    feat: upgrade dev playground
    - setup auto import from vue and gitart-vue-dialog
    - setup auto import gitart-vue-dialog components

--- packages/dialog/dev/vite.config.ts
+++ packages/dialog/dev/vite.config.ts
@@ -2,6 +2,8 @@
 import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import WindiCSS from 'vite-plugin-windicss'
 
 const resolve = (str: string) => path.resolve(__dirname, str)
@@ -11,6 +13,33 @@ export default defineConfig({
 
   plugins: [
     vue(),
+    AutoImport({
+      imports: [
+        'vue',
+        {
+          'gitart-vue-dialog': [
+            'dialogInjectionKey',
+            ['plugin', 'dialogPlugin'],
+          ],
+        },
+      ],
+      dts: resolve('auto-imports.d.ts'),
+    }),
+    Components({
+      resolvers: [
+        (name: string) => {
+          if ([
+            'GDialog',
+            'GDialogRoot',
+          ].includes(name)) {
+            return { importName: name, path: 'gitart-vue-dialog' }
+          }
+
+          return
+        },
+      ],
+      dts: resolve('components.d.ts'),
+    }),
     WindiCSS(),
   ],
 
