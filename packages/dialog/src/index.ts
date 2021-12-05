c975b4d163d8aa442ba0a431b70d1ba74160e5ac
Michael.Kryvoruchko <Michael.Kryvoruchko@mdm.de>
2021-12-06 00:35:29 +0200

    fix: return exporting IDialog interface

--- packages/dialog/src/index.ts
+++ packages/dialog/src/index.ts
@@ -4,6 +4,10 @@ import './scss/main.scss'
 export { default as GDialog } from './components/GDialog.vue'
 
 // using plugin
+export type {
+  IDialog,
+} from './types/Plugin'
+
 export { default as GDialogRoot } from './components/GDialogRoot.vue'
 
 export {
